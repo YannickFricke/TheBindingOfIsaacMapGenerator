fe81ee620aad960284a4ef25c15882d96ef10097
markusbink <bink-markus@web.de>
2020-01-29 23:02:50 +0100

    Add fixed sidebar closes #13
    Made sidebars fixed and add overflow to main grid area

--- src/App.tsx
+++ src/App.tsx
@@ -8,56 +8,72 @@ import { IColumnContent } from './IColumnContent';
 import ReactTooltip from 'react-tooltip';
 
 const StyledApplication = styled.div`
-        display: grid;
-        grid-template-areas: 'header header header header header header'
-                            'left main main main main right';
+  display: grid;
+  grid-template-columns: 300px auto 300px;
+  grid-template-areas:
+    "header header header"
+    "left main right";
 `;
 
 const GridContainer = styled.div`
-    grid-area: main;
-    display: grid;
+  grid-area: main;
+  display: grid;
+  overflow: scroll;
+  height: calc(100vh - 38px);
 `;
 
 export interface ISelectionType {
-    type: 'shape' | 'type' | '';
-    id: string;
+  type: 'shape' | 'type' | '';
+  id: string;
 }
 
 export const App: React.FC = () => {
-    const [rows, setRows] = useState(10);
-    const [columns, setColumns] = useState(10);
-    const [grid, setGrid] = useState<IColumnContent[][]>([]);
-    const [selection, setSelection] = useState<ISelectionType>({
-        type: '',
-        id: '',
-    });
+  const [rows, setRows] = useState(10);
+  const [columns, setColumns] = useState(10);
+  const [grid, setGrid] = useState<IColumnContent[][]>([]);
+  const [selection, setSelection] = useState<ISelectionType>({
+    type: '',
+    id: ''
+  });
 
-    useEffect(() => {
-        const tempGrid: IColumnContent[][] = [];
+  useEffect(() => {
+    const tempGrid: IColumnContent[][] = [];
 
-        for (let row = 0; row < rows; row++) {
-            const tempRow = [];
+    for (let row = 0; row < rows; row++) {
+      const tempRow = [];
 
-            for (let column = 0; column < columns; column++) {
-                tempRow.push({
-                    shape: '',
-                    type: '',
-                });
-            }
+      for (let column = 0; column < columns; column++) {
+        tempRow.push({
+          shape: '',
+          type: ''
+        });
+      }
 
-            tempGrid.push(tempRow);
-        }
+      tempGrid.push(tempRow);
+    }
 
-        setGrid(tempGrid);
-    }, [rows, columns]);
+    setGrid(tempGrid);
+  }, [rows, columns]);
 
-    return <StyledApplication id="application">
-        <Header/>
-        <LeftSidebar setSelection={setSelection}/>
-        <GridContainer>
-            <Grid grid={grid} setGrid={setGrid} selection={selection}/>
-            <ReactTooltip/>
-        </GridContainer>
-        <RightSidebar rows={rows} setRows={setRows} columns={columns} setColumns={setColumns}/>
-    </StyledApplication>;
+  return (
+    <StyledApplication id="application">
+      <Header />
+      <LeftSidebar setSelection={setSelection} />
+      <GridContainer>
+        <Grid
+          columns={columns}
+          grid={grid}
+          setGrid={setGrid}
+          selection={selection}
+        />
+        <ReactTooltip />
+      </GridContainer>
+      <RightSidebar
+        rows={rows}
+        setRows={setRows}
+        columns={columns}
+        setColumns={setColumns}
+      />
+    </StyledApplication>
+  );
 };
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -6,118 +6,115 @@ import { canSetShape, canSetType, updateGrid } from '../Helper';
 import { Images } from '../Images';
 
 const StyledGrid = styled.div`
-    border: 1px solid #b0b0b0;
-    align-self: center;
-    justify-self: center;
+  border: 1px solid #b0b0b0;
+  align-self: center;
+  justify-self: center;
+  width: calc(${props => props.columns} * 37px);
 `;
 
 const GridRow = styled.div`
-    height: 32px;
-    border-top: 1px solid #b0b0b0;
+  height: 32px;
+  border-top: 1px solid #b0b0b0;
 
-    &:nth-child(1) {
-        border: none;
-    }
+  &:nth-child(1) {
+    border: none;
+  }
 `;
 
 const GridColumn = styled.div`
-    width: 36px;
-    height: 32px;
-    float: left;
-    border-left: 1px solid #b0b0b0;
+  width: 36px;
+  height: 32px;
+  float: left;
+  border-left: 1px solid #b0b0b0;
 
-    &:nth-child(1) {
-        border: none;
-    }
+  &:nth-child(1) {
+    border: none;
+  }
 `;
 
 const RoomTypeImage = styled.img`
-    width: 60%;
-    height: 60%;
-    margin-top: 7px;
-    margin-left: 7px;
-    pointer-events: none;
+  width: 60%;
+  height: 60%;
+  margin-top: 7px;
+  margin-left: 7px;
+  pointer-events: none;
 `;
 
 interface IGridProps {
-    grid: IColumnContent[][];
-    setGrid: React.Dispatch<React.SetStateAction<IColumnContent[][]>>;
-    selection: ISelectionType;
+  grid: IColumnContent[][];
+  columns: number;
+  setGrid: React.Dispatch<React.SetStateAction<IColumnContent[][]>>;
+  selection: ISelectionType;
 }
 
-export const Grid: React.FC<IGridProps> = (props) => {
-    const handleColumnClick = (event: React.MouseEvent<HTMLDivElement>): void => {
-        if (props.selection.type === '') {
-            return;
+export const Grid: React.FC<IGridProps> = props => {
+  const handleColumnClick = (event: React.MouseEvent<HTMLDivElement>): void => {
+    if (props.selection.type === '') {
+      return;
+    }
+
+    let tempGrid: IColumnContent[][] = [...props.grid];
+    const selectedId = props.selection.id;
+    const target = event.target as HTMLDivElement;
+    const rowIndex = parseInt(target.dataset['row'] || '', 10);
+    const columnIndex = parseInt(target.dataset['column'] || '', 10);
+
+    if (isNaN(rowIndex) || isNaN(columnIndex)) {
+      return;
+    }
+
+    switch (props.selection.type) {
+      case 'shape':
+        if (!canSetShape(selectedId, rowIndex, columnIndex, tempGrid)) {
+          return;
         }
 
-        let tempGrid: IColumnContent[][] = [
-            ...props.grid,
-        ];
-        const selectedId = props.selection.id;
-        const target = event.target as HTMLDivElement;
-        const rowIndex = parseInt(target.dataset['row'] || '', 10);
-        const columnIndex = parseInt(target.dataset['column'] || '', 10);
+        tempGrid = updateGrid(selectedId, rowIndex, columnIndex, tempGrid);
 
-        if (isNaN(rowIndex) || isNaN(columnIndex)) {
-            return;
+        break;
+
+      case 'type':
+        if (!canSetType(rowIndex, columnIndex, tempGrid)) {
+          return;
         }
 
-        switch (props.selection.type) {
-            case 'shape':
-                if (!canSetShape(
-                    selectedId,
-                    rowIndex,
-                    columnIndex,
-                    tempGrid,
-                )) {
-                    return;
-                }
+        tempGrid[rowIndex][columnIndex].type = selectedId;
+        break;
+    }
 
-                tempGrid = updateGrid(
-                    selectedId,
-                    rowIndex,
-                    columnIndex,
-                    tempGrid,
-                );
+    props.setGrid(tempGrid);
+  };
 
-                break;
-
-            case 'type':
-                if (!canSetType(
-                    rowIndex,
-                    columnIndex,
-                    tempGrid,
-                )) {
-                    return;
-                }
-
-                tempGrid[rowIndex][columnIndex].type = selectedId;
-                break;
-        }
-
-        props.setGrid(tempGrid);
-    };
-
-    return <StyledGrid>
-        {props.grid.map((row: IColumnContent[], rowIndex: number) => {
-            return <GridRow key={rowIndex}>
-                {row.map((column: IColumnContent, columnIndex: number) => {
-                    return <GridColumn key={columnIndex}
-                                       onClick={handleColumnClick}
-                                       data-row={rowIndex}
-                                       data-column={columnIndex}
-                                       style={{
-                                           backgroundImage: column.shape !== '' ?
-                                               // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                               `url("${(Images.shapeSprites as any)[column.shape]}")` : '',
-                                       }}
-                    >
-                        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-                        {column.type !== '' && <RoomTypeImage src={(Images.types as any)[column.type]}/>}
-                    </GridColumn>;
-                })}
-            </GridRow>;
-        })}
-    </StyledGrid>;
+  return (
+    <StyledGrid columns={props.columns}>
+      {props.grid.map((row: IColumnContent[], rowIndex: number) => {
+        return (
+          <GridRow key={rowIndex}>
+            {row.map((column: IColumnContent, columnIndex: number) => {
+              return (
+                <GridColumn
+                  key={columnIndex}
+                  onClick={handleColumnClick}
+                  data-row={rowIndex}
+                  data-column={columnIndex}
+                  style={{
+                    backgroundImage:
+                      column.shape !== ''
+                        ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          `url("${(Images.shapeSprites as any)[column.shape]}")`
+                        : ''
+                  }}
+                >
+                  {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+                  {column.type !== '' && (
+                    <RoomTypeImage src={(Images.types as any)[column.type]} />
+                  )}
+                </GridColumn>
+              );
+            })}
+          </GridRow>
+        );
+      })}
+    </StyledGrid>
+  );
 };
